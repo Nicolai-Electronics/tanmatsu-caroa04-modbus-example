144738ce6da394ce0dcca8549accd85b22966e16
Renze Nicolai <renze@rnplus.nl>
2026-04-24 02:38:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tanmatsu example project: Modbus
+
+This demonstration app shows how to control a CAROA04 I/O card over Modbus using the CATT to RS485 and TWAI/CAN bus adapter on Tanmatsu.
+
+## References
+
+- [I/O card](https://eletechsup.com/products/caroa04-carob08-caroc16-4-8-16-di-do-can-relay-controller-module-rs485-digital-npn-pnp-io-expanding-board-for-cnc-car-automated-industry-caro?_pos=1&_sid=cb43204cd&_ss=r)
+- [I/O card protocol (download link, rar archive)](https://onedrive.live.com/?redeem=aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBdjRQTHhIX3o4ZjFxelhyMVJzQWVCLWR4UTJtP2U9M09idHZO&cid=F5C7CFFF112F0FFE&id=F5C7CFFF112F0FFE%215557&parId=F5C7CFFF112F0FFE%21105&o=OneUp)
+
+## License
+
+The contents of this repository may be considered in the public domain or [CC0-1.0](https://creativecommons.org/publicdomain/zero/1.0) licensed at your disposal.
+
+At Nicolai Electronics we love open source so we recommend licensing your work based on this template under terms of the [MIT license](https://opensource.org/license/mit). The MIT license allows others to build upon your work without restrictions while also making sure you retain your attribution.
---
+++ main/main.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include "bsp/device.h"
+#include "bsp/display.h"
+#include "bsp/input.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/projdefs.h"
+#include "hal/lcd_types.h"
+#include "hal/uart_types.h"
+#include "modbus.h"
+#include "nvs_flash.h"
+#include "pax_fonts.h"
+#include "pax_gfx.h"
+#include "pax_text.h"
+
+// Constants
+static char const TAG[] = "main";
+
+// Global variables
+static size_t                       display_h_res        = 0;
+static size_t                       display_v_res        = 0;
+static lcd_color_rgb_pixel_format_t display_color_format = LCD_COLOR_PIXEL_FORMAT_RGB565;
+static lcd_rgb_data_endian_t        display_data_endian  = LCD_RGB_DATA_ENDIAN_LITTLE;
+static pax_buf_t                    fb                   = {0};
+static QueueHandle_t                input_event_queue    = NULL;
+
+void blit(void) {
+    bsp_display_blit(0, 0, display_h_res, display_v_res, pax_buf_get_pixels(&fb));
+}
+
+void app_main(void) {
+    // Start the GPIO interrupt service
+    gpio_install_isr_service(0);
+
+    // Initialize the Non Volatile Storage partition
+    esp_err_t res = nvs_flash_init();
+    if (res == ESP_ERR_NVS_NO_FREE_PAGES || res == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        res = nvs_flash_erase();
+        if (res != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to erase NVS flash: %d", res);
+            return;
+        }
+        res = nvs_flash_init();
+    }
+    if (res != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize NVS flash: %d", res);
+        return;
+    }
+
+    // Initialize the Board Support Package
+    const bsp_configuration_t bsp_configuration = {
+        .display =
+            {
+                .requested_color_format = LCD_COLOR_PIXEL_FORMAT_RGB888,
+                .num_fbs                = 1,
+            },
+    };
+    res = bsp_device_initialize(&bsp_configuration);
+    if (res != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize BSP: %d", res);
+        return;
+    }
+
+    // Get display parameters and rotation
+    res = bsp_display_get_parameters(&display_h_res, &display_v_res, &display_color_format, &display_data_endian);
+    if (res != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get display parameters: %d", res);
+        return;
+    }
+
+    // Convert ESP-IDF color format into PAX buffer type
+    pax_buf_type_t format = PAX_BUF_24_888RGB;
+    switch (display_color_format) {
+        case LCD_COLOR_PIXEL_FORMAT_RGB565:
+            format = PAX_BUF_16_565RGB;
+            break;
+        case LCD_COLOR_PIXEL_FORMAT_RGB888:
+            format = PAX_BUF_24_888RGB;
+            break;
+        default:
+            break;
+    }
+
+    // Convert BSP display rotation format into PAX orientation type
+    bsp_display_rotation_t display_rotation = bsp_display_get_default_rotation();
+    pax_orientation_t      orientation      = PAX_O_UPRIGHT;
+    switch (display_rotation) {
+        case BSP_DISPLAY_ROTATION_90:
+            orientation = PAX_O_ROT_CCW;
+            break;
+        case BSP_DISPLAY_ROTATION_180:
+            orientation = PAX_O_ROT_HALF;
+            break;
+        case BSP_DISPLAY_ROTATION_270:
+            orientation = PAX_O_ROT_CW;
+            break;
+        case BSP_DISPLAY_ROTATION_0:
+        default:
+            orientation = PAX_O_UPRIGHT;
+            break;
+    }
+
+    // Initialize graphics stack
+    pax_buf_init(&fb, NULL, display_h_res, display_v_res, format);
+    pax_buf_reversed(&fb, display_data_endian == LCD_RGB_DATA_ENDIAN_BIG);
+    pax_buf_set_orientation(&fb, orientation);
+
+#define BLACK 0xFF000000
+#define WHITE 0xFFFFFFFF
+#define RED   0xFFFF0000
+
+    // Get input event queue from BSP
+    ESP_ERROR_CHECK(bsp_input_get_queue(&input_event_queue));
+
+    // Main section of the app
+
+    ESP_ERROR_CHECK(modbus_driver_install(UART_NUM_0, 34, 5, 3, 9600, UART_PARITY_DISABLE, UART_STOP_BITS_1));
+
+    pax_simple_rect(&fb, WHITE, 0, 0, pax_buf_get_width(&fb), 72);
+    pax_draw_text(&fb, BLACK, pax_font_sky_mono, 16, 0, 0, "Starting...");
+    blit();
+
+    while (1) {
+        for (uint8_t i = 0; i <= 0x0F; i++) {
+
+            esp_err_t write_coils_res = modbus_write_multiple_coils(UART_NUM_0, 0x01, 0, 4, (uint8_t[]){i}, 1000);
+            printf("Writing coils (0x%02x): %s\r\n", i, esp_err_to_name(write_coils_res));
+
+            uint8_t   coils          = 0;
+            esp_err_t read_coils_res = modbus_read_coils(UART_NUM_0, 0x01, 0, 4, &coils, 1000);
+            printf("Reading coils       : %s = 0x%02x\r\n", esp_err_to_name(read_coils_res), coils);
+
+            uint8_t   inputs          = 0;
+            esp_err_t read_inputs_res = modbus_read_discrete_inputs(UART_NUM_0, 0x01, 0, 4, &inputs, 1000);
+            printf("Reading inputs      : %s = 0x%02x\r\n", esp_err_to_name(read_inputs_res), inputs);
+
+            printf("\r\n");
+
+            pax_simple_rect(&fb, WHITE, 0, 0, pax_buf_get_width(&fb), 72);
+            pax_draw_text(&fb, BLACK, pax_font_sky_mono, 16, 0, 0, "Result:");
+            char text[64];
+            snprintf(text, sizeof(text), "Writing coils (0x%02x): %s", i, esp_err_to_name(write_coils_res));
+            pax_draw_text(&fb, BLACK, pax_font_sky_mono, 16, 0, 18, text);
+            snprintf(text, sizeof(text), "Reading coils       : %s = 0x%02x", esp_err_to_name(read_coils_res), coils);
+            pax_draw_text(&fb, BLACK, pax_font_sky_mono, 16, 0, 36, text);
+            snprintf(text, sizeof(text), "Reading inputs      : %s = 0x%02x", esp_err_to_name(read_inputs_res), inputs);
+            pax_draw_text(&fb, BLACK, pax_font_sky_mono, 16, 0, 54, text);
+            blit();
+
+            vTaskDelay(pdMS_TO_TICKS(500));
+        }
+    }
+}
